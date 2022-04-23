3cfe7ebd9299ce02ce69b9160cfabb63511d1d4d
frake23 <dimsacom@gmail.com>
2022-04-24 02:25:14 +0300

    fixes

--- client/src/components/test-form.svelte
+++ client/src/components/test-form.svelte
@@ -7,7 +7,7 @@
 	let files: FileList;
 
 	const onSubmit = async () => {
-		if (!files?.length) return;
+		if (!files?.length || !link || !name || files[0].type !== 'application/json') return;
 
 		await fetch('/api/test', {
 			method: 'POST',
@@ -19,6 +19,7 @@
 		const tests = await res.json();
 
 		$testStore.tests = tests;
+		$testStore.selectedTestIndex = 0;
 	};
 
 	let showModal = false;
@@ -48,7 +49,18 @@
 					placeholder="Название теста"
 				/>
 				<input class="focus:ring border p-1 rounded" bind:value={link} placeholder="Ссылка" />
-				<input class="focus:ring border p-1 rounded" accept="json" bind:files type="file" />
+				<input
+					class={`block w-full text-sm text-slate-500
+                        file:mr-4 file:py-2 file:px-4
+                        file:rounded-full file:border-0
+                        file:text-sm file:font-semibold
+                        file:bg-blue-50 file:text-blue-700
+                        hover:file:bg-blue-100
+                    `}
+					accept="application/json"
+					bind:files
+					type="file"
+				/>
 				<button
 					class="rounded-lg p-2 bg-blue-500 hover:bg-blue-600 transition-all text-white flex items-center justify-center"
 					on:click={onSubmit}
